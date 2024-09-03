ab101ed90ad3551cc92b1e426d65af9ac90d684c
Yasmine Ghanem <74925701+yasmineghanem@users.noreply.github.com>
2024-09-03 17:35:55 +0300

    linked lists

---
+++ Data Structures/Linked Lists/get_node.py
@@ -0,0 +1,96 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+class SinglyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert_node(self, node_data):
+        node = SinglyLinkedListNode(node_data)
+
+        if not self.head:
+            self.head = node
+        else:
+            self.tail.next = node
+
+        self.tail = node
+
+
+def print_singly_linked_list(node, sep, fptr):
+    while node:
+        fptr.write(str(node.data))
+
+        node = node.next
+
+        if node:
+            fptr.write(sep)
+
+#
+# Complete the 'getNode' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER_SINGLY_LINKED_LIST llist
+#  2. INTEGER positionFromTail
+#
+
+#
+# For your reference:
+#
+# SinglyLinkedListNode:
+#     int data
+#     SinglyLinkedListNode next
+#
+#
+
+
+def getNode(llist, positionFromTail):
+    # Write your code here
+    temp_list = []
+    current = llist
+    while current:
+        temp_list.append(current)
+        current = current.next
+
+    position = len(temp_list) - positionFromTail - 1
+    print('--------------------------')
+    print(len(temp_list))
+    print(positionFromTail)
+    print(position)
+
+    node = temp_list[position]
+
+    return node.data
+
+
+if __name__ == '__main__':
+
+    tests = int(input())
+
+    for tests_itr in range(tests):
+        llist_count = int(input())
+
+        llist = SinglyLinkedList()
+
+        for _ in range(llist_count):
+            llist_item = int(input())
+            llist.insert_node(llist_item)
+
+        position = int(input())
+
+        result = getNode(llist.head, position)
+
+        print(result)
